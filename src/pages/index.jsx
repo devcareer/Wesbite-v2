337613746e70a05eac76063696da5852ac3d91d0
everich4040 <megamindcooperations@gmail.com>
2020-09-29 12:08:52 +0000

    footer component added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import DefaultLayout from "../layouts/DefaultLayout"
+import Footer from "../layouts/Footer"
 
 export default function Home() {
   return (
     <DefaultLayout>
-      <div>Hello world!</div>
+      <Footer />
     </DefaultLayout>
   )
 }
