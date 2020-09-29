efe4c43d0058c84197b318ca208281f2f37eb183
gfellah45 <jerry.aaron45@gmail.com>
2020-09-29 14:02:47 +0100

    bg-fixed-issues-raised

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,30 +4,29 @@ function Card({ name, image, title, socials }) {
   return (
     <>
       <div className="card">
-        <img src={image} className="card-img" alt={name} />
-        <div className="card-body">
-          <p className="card-title">{name}</p>
-          <p className="card-text">{title}</p>
-          <p className="card-company">DevCareer</p>
-          <div>
-            {socials.map((each, idx) => (
-              <div className="card-socials" key={idx}>
-                {" "}
-                <a href={each.twitter} target="_blank">
-                  <img
-                    src="https://res.cloudinary.com/gfellah45/image/upload/v1601374991/img/twitter_tqfwrj.png"
-                    alt="twitter"
-                  />
-                </a>
-                <a href={each.github} target="_blank">
-                  <img
-                    src="https://res.cloudinary.com/gfellah45/image/upload/v1601374976/img/github_typexk.png"
-                    alt="github"
-                  />
-                </a>
-              </div>
-            ))}
-          </div>
+        <img src={image} className="card__img" alt={name} />
+        <div className="card__body">
+          <p className="card__title">{name}</p>
+          <p className="card__text">{title}</p>
+          <p className="card__company">DevCareer</p>
+
+          {socials.map((each, idx) => (
+            <div className="card__socials" key={idx}>
+              {" "}
+              <a href={each.twitter} target="_blank">
+                <img
+                  src="https://res.cloudinary.com/gfellah45/image/upload/v1601374991/img/twitter_tqfwrj.png"
+                  alt="twitter"
+                />
+              </a>
+              <a href={each.github} target="_blank">
+                <img
+                  src="https://res.cloudinary.com/gfellah45/image/upload/v1601374976/img/github_typexk.png"
+                  alt="github"
+                />
+              </a>
+            </div>
+          ))}
         </div>
       </div>
     </>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,6 @@
 import React from "react"
 import DefaultLayout from "../layouts/DefaultLayout"
-import Footer from "../layouts/Footer"
 
 export default function Home() {
-<<<<<<< HEAD
-  return <DefaultLayout></DefaultLayout>
-=======
-  return (
-    <DefaultLayout>
-      <Footer />
-    </DefaultLayout>
-  )
->>>>>>> 337613746e70a05eac76063696da5852ac3d91d0
+  return <DefaultLayout>Hello World!</DefaultLayout>
 }
