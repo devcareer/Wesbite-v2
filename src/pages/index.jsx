d6a44528d71b710119adf7d0638eb4e8fceba53a
kingflamez <flamekeed@gmail.com>
2020-09-29 14:21:43 +0100

    fixes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,10 @@
 import React from "react"
 import DefaultLayout from "../layouts/DefaultLayout"
-import Footer from "../layouts/high-risers"
 
 export default function Home() {
   return (
     <DefaultLayout>
-      <Footer />
+      Hello World
     </DefaultLayout>
   )
 }
