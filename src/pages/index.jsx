473909fc7195100f6d8e543b043eb67de92e9604
bint <bintmuhd247@gmail.com>
2020-09-30 10:40:14 +0100

    bg-fixed-media-queries

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import DefaultLayout from "../layouts/DefaultLayout"
+import Footer from "../components/global/Footer"
 
 export default function Home() {
   return (
     <DefaultLayout>
-      Hello World
+      <Footer />
     </DefaultLayout>
   )
 }
