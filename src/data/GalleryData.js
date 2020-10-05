ce8d84c0447ab60bd6c7dd3cd6d940cfe5e67d73
Yussif <djiceman.gh@gmail.com>
2020-10-05 16:51:15 +0000

    All gallery errors fixed

---
+++ src/data/GalleryData.js
@@ -0,0 +1,32 @@
+export const gallery = [
+  {
+    col: "col-12 col-sm-7",
+    src: "https://res.cloudinary.com/netkid/image/upload/v1601634906/dev-careers/13_vefdl4.jpg",
+    text: "Dev Career Interns in Nigeria"
+  },
+  {
+    col: "col-12 col-sm-5",
+    src: "https://res.cloudinary.com/netkid/image/upload/v1601634918/dev-careers/image_from_ios-2_lh1b8x.jpg",
+    text: "Awesome Dev Career Interns"
+  },
+  {
+    col: "col-12 col-sm-5",
+    src: "https://res.cloudinary.com/netkid/image/upload/v1601634923/dev-careers/20200806_130106_kdytkz.jpg",
+    text: "Interns at Wennovation Hub, Nigeria busy coding"
+  },
+  {
+    col: "col-12 col-sm-7",
+    src: "https://res.cloudinary.com/netkid/image/upload/v1601634907/dev-careers/IMG-20200806-WA0037_cl04qt.jpg",
+    text: "Dev Career Interns in Kumasi, Ghana"
+  },
+  {
+    col: "col-12 col-sm-7",
+    src: "https://res.cloudinary.com/netkid/image/upload/v1601634906/dev-careers/IMG-20200804-WA0022_umv1is.jpg",
+    text: "Dev Career Interns at Swahilipot Hub, Kenya"
+  },
+  {
+    col: "col-12 col-sm-5",
+    src: "https://res.cloudinary.com/netkid/image/upload/v1601635407/dev-careers/IMG_20200617_152113_qk1xtv.jpg",
+    text: "Laptops Ready for Dev Careers Cohort Two"
+  }
+];
---
+++ src/pages/gallery.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+import DefaultLayout from "../layouts/DefaultLayout"
+import Hero from "../components/Hero"
+import GalleryImgs from "../components/GalleryImgs"
+import { gallery } from "../data/GalleryData"
+
+export default function Gallery() {
+  return (
+    <DefaultLayout>
+      <Hero title="Gallery" />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-12 col-sm-8 offset-sm-2">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
+              explicabo inventore culpa natus mollitia veniam quia alias
+              voluptates sapiente incidunt.
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          {gallery.map(img => {
+            return (
+              <GalleryImgs
+                key={img.text}
+                col={img.col}
+                src={img.src}
+                text={img.text}
+              />
+            )
+          })}
+        </div>
+      </div>
+    </DefaultLayout>
+  )
+}
