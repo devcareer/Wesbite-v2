006aad1c773823a40b252ec7116ab01ecc134dc3
Onifade Dolapo <hardolipey@gmail.com>
2020-10-06 15:58:11 +0100

    accordion top-div padding

--- src/components/pages/gallery/GalleryImgs.jsx
+++ src/components/pages/gallery/GalleryImgs.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function GalleryImgs({col, src, text}) {
   return (
     <div className={col}>
-      <img className="col-12" src={src} alt={text}/>
+      <img className=" gallery__img col-12 " src={src} alt={text}/>
       <p className="gallery__desc">{text}</p>
     </div>
   );
