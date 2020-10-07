b94d31608402eb0246f61091fa1eb5b252ae14c8
Broma Gift <elenwogift2@gmail.com>
2020-10-07 13:10:08 +0100

    removed duplicate candidate card and Accordion imports

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import DefaultLayout from "../layouts/DefaultLayout"
 import Hero from "../components/global/Hero";
-import Accordion from "../components/Accordion"
 import Accordion from "../components/pages/faq/Accordion"
 
 export default function Home() {
