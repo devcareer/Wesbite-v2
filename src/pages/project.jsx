c193736da06bd283694728e829e034bd43b80bac
Chidi Okoye <okoyecb@gmail.com>
2020-12-07 14:03:44 +0100

    Image overlap fixed

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,12 +1,10 @@
 import React from "react"
 import Hero from "../components/global/Hero"
-
 import DefaultLayout from "../layouts/DefaultLayout"
 import ImageCard from "../components/pages/project/ImageCard"
 import Content from "../components/pages/project/Content"
 import Img from "../assets/images/proj-v2.png"
 import Img3 from "../assets/images/testing.png"
-import Img2 from "../assets/images/project-v1.png"
 import ProjectData from "../data/ProjectsData.js"
 
 function project() {
@@ -52,23 +50,6 @@ function project() {
               </div>
             </div>
           </div>
-
-          {/* <div className="row  mb-4 container m-auto">
-            <div className="col-12 col-md-6">
-              <ImageCard imgOne={Img} imgTwo={Img2} />
-            </div>
-
-            <div className="col-12 col-md-6">
-              <div className="project-card">
-                <Content
-                  id={ProjectData[0].id}
-                  subHeading={ProjectData[0].sub}
-                  heading={ProjectData[0].heading}
-                  desc={ProjectData[0].desc}
-                />
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
     </DefaultLayout>
